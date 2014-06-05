b9889fa0af4316212130b7258dd94a83b8f63a0a
M Yusuf Bachtiar <ario@localhost.localdomain>
2014-06-05 11:11:36 +0700

    Selesai untuk halaman about

---
+++ application/views/about/about.php
@@ -0,0 +1,16 @@
+	<link href="<?php echo base_url(); ?>media/css/bootstrap.min.css" rel="stylesheet">
+
+	<div class="container">
+		<div class="well">
+			<div class="text-center">
+				<h1>Aplikasi SMS Gateway berbasis WEB</h1>
+				<h1>piSMS versi 1.0</h1>
+			</div>
+			<p>Lisensi : GNU/GPL<br>
+				Kontak Pengembang : <a href="http://www.artikulpi.com" target="_blank">artikulpi.com</a><br>
+				Github : 
+				Jika terdapat bug pada aplikasi ini, maka sila kirim feedback pada link berikut <?php echo mailto('office@artikulpi.com', 'ini'); ?>
+			</p>
+			<a href="<?=site_url('admin')?>">Kembali ke halaman pengelolaan sms</a>
+		</div>
+	</div>
--- application/views/template/layout.php
+++ application/views/template/layout.php
@@ -35,51 +35,51 @@
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="sr-only">Toggle navigation</span></button><a class="navbar-brand" href=""><img src="/pisms/media/img/SMS.png" class="img-rounded" width="30">piSMS </a>
 				</div>
 				<div class="navbar-collapse collapse">
-								<ul class="nav navbar-nav navbar-right">
-								                                    <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Account
-								                                        <b class="caret"></b></a>
-								                                        <ul class="dropdown-menu">
-								                                            <li>
-								<li><a style="margin-top: 15px" class="btn" href="<?=site_url('auth/logout');?>"><i class="glyphicon glyphicon-user"></i> <?=$this->session->userdata('user');?> | Logout <i class="glyphicon glyphicon-lock"></i></a></li>
-							</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Account
+							<b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								<li>
+									<li><a style="margin-top: 15px" class="btn" href="<?=site_url('auth/logout');?>"><i class="glyphicon glyphicon-user"></i> <?=$this->session->userdata('user');?> | Logout <i class="glyphicon glyphicon-lock"></i></a></li>
+								</ul>
+							</div>
 						</div>
 					</div>
-		</div>
-		
-		<div class="login"></div>
-		<!-- List Menu -->
-		
-		<div class="list">
-			
-			<div class="list-group">
-				
-				<a style="background-color: #006E7D; border-color:transparent;" class="list-group-item active">List Menu</a>
-				<a href="<?=site_url('admin')?>" class="list-group-item"><i class="glyphicon glyphicon-home"></i> Dashboard</a>
-				<a href="<?=site_url('sms/create')?>" class="list-group-item"><i class="glyphicon glyphicon-file"></i> Tulis Pesan</a>
-				<a href="<?=site_url('sms/inbox')?>" class="list-group-item"><i class="glyphicon glyphicon-tasks"></i> Inbox</a>
-				<a href="<?=site_url('sms/outbox')?>" class="list-group-item"><i class="glyphicon glyphicon-book"></i> Outbox</a>
-				<a href="<?=site_url('sms/sentitem')?>" class="list-group-item"><i class="glyphicon glyphicon-book"></i> Sent Item</a>
-				<br>
-				<a style="background-color: #006E7D; border-color:transparent;" class="list-group-item active">List Menu</a>
-				<a href="<?=site_url('contact')?>" class="list-group-item"><i class="glyphicon glyphicon-file"></i> Kontak</a>
-				<a href="<?=site_url('pigroup')?>" class="list-group-item"><i class="glyphicon glyphicon-tasks"></i> Grup</a>
-				<a href="<?=site_url('contactgroup')?>" class="list-group-item"><i class="glyphicon glyphicon-tasks"></i> Anggota Grup</a>
-				<br>
-				<a style="background-color: #006E7D; border-color:transparent;" class="list-group-item active">List Menu</a>
-				<a href="<?=site_url('log_activity')?>" class="list-group-item"><i class="glyphicon glyphicon-th-list"></i> Log Aktifitas</a>
-				<?php if($this->session->userdata('role')==1){?>
-				<a href="<?=site_url('user')?>" class="list-group-item"><i class="glyphicon glyphicon-th-list"></i> List User</a>
-				<?php
-			}
-			?>
-			<a href="<?=site_url('user/reset/'.$this->session->userdata('id'));?>" class="list-group-item"><i class="glyphicon glyphicon-check"></i> Ubah Password <strong>[ <?=$this->session->userdata('user')?> ]</strong></a>
+					
+					<div class="login"></div>
+					<!-- List Menu -->
+					
+					<div class="list">
+						
+						<div class="list-group">
+							
+							<a style="background-color: #006E7D; border-color:transparent;" class="list-group-item active">List Menu</a>
+							<a href="<?=site_url('admin')?>" class="list-group-item"><i class="glyphicon glyphicon-home"></i> Dashboard</a>
+							<a href="<?=site_url('sms/create')?>" class="list-group-item"><i class="glyphicon glyphicon-file"></i> Tulis Pesan</a>
+							<a href="<?=site_url('sms/inbox')?>" class="list-group-item"><i class="glyphicon glyphicon-tasks"></i> Inbox</a>
+							<a href="<?=site_url('sms/outbox')?>" class="list-group-item"><i class="glyphicon glyphicon-book"></i> Outbox</a>
+							<a href="<?=site_url('sms/sentitem')?>" class="list-group-item"><i class="glyphicon glyphicon-book"></i> Sent Item</a>
+							<br>
+							<a style="background-color: #006E7D; border-color:transparent;" class="list-group-item active">List Menu</a>
+							<a href="<?=site_url('contact')?>" class="list-group-item"><i class="glyphicon glyphicon-file"></i> Kontak</a>
+							<a href="<?=site_url('pigroup')?>" class="list-group-item"><i class="glyphicon glyphicon-tasks"></i> Grup</a>
+							<a href="<?=site_url('contactgroup')?>" class="list-group-item"><i class="glyphicon glyphicon-tasks"></i> Anggota Grup</a>
+							<br>
+							<a style="background-color: #006E7D; border-color:transparent;" class="list-group-item active">List Menu</a>
+							<a href="<?=site_url('log_activity')?>" class="list-group-item"><i class="glyphicon glyphicon-th-list"></i> Log Aktifitas</a>
+							<?php if($this->session->userdata('role')==1){?>
+							<a href="<?=site_url('user')?>" class="list-group-item"><i class="glyphicon glyphicon-th-list"></i> List User</a>
+							<?php
+						}
+						?>
+						<a href="<?=site_url('user/reset/'.$this->session->userdata('id'));?>" class="list-group-item"><i class="glyphicon glyphicon-check"></i> Ubah Password <strong>[ <?=$this->session->userdata('user')?> ]</strong></a>
 			<!--a href="" class="list-group-item"><i class="glyphicon glyphicon-tag"></i> Page</a>
 			<a href="" class="list-group-item"><i class="glyphicon glyphicon-user"></i> Pengguna</a>
 			<a href="" class="list-group-item"><i class="glyphicon glyphicon-tags"></i> Media Manager</a-->
 			</div>
 		</div>
-	
-	
+		
+		
 		<div class="content content-top">
 			<div class="panel-body">
 				<div class="main-admin">
@@ -113,20 +113,20 @@
 					</p>
 				</div>
 			</div>
-			</div>
-  		  
-    
-	<div class="navbar navbar-default navbar-fixed-bottom" role="navigation">
-		<div class="container-fluid">
-	      <p class="navbar-text pull-left">© pisms-2014
-	      </p>
-			<div class="navbar-collapse collapse">
-		  		      	<a href="#"><p class="navbar-text pull-right">About Us
-		  		      </p>
-		  		      	<a href="#"><p class="navbar-text pull-right">Contact
-		  		      </p>	
-					</div>
+		</div>
+		
+		
+		<div class="navbar navbar-default navbar-fixed-bottom" role="navigation">
+			<div class="container-fluid">
+				<p class="navbar-text pull-left">© pisms-2014
+				</p>
+				<div class="navbar-collapse collapse">
+					<a href="<?=site_url('about')?>"><p class="navbar-text pull-right">About Us
+					</p>
+					<a href="#"><p class="navbar-text pull-right">Contact
+					</p>	
 				</div>
+			</div>
+		</div>
 	</div>
-			  </div>
-			
+	
