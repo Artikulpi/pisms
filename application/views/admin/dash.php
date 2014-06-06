2e23fba5917cbf561037dfc9a21a40824b67c5c0
nazih14 <nazihbopas@gmail.com>
2014-06-06 22:53:00 +0700

    Update view data

--- application/views/admin/dash.php
+++ application/views/admin/dash.php
@@ -7,46 +7,44 @@
 	</div>
 <hr>
 
-	<div class="row">
-		<div class="col-sm-3">
+	<div class="row destacados">
+		<div class="col-md-4">
     	    <div class="hero-widget well well-sm">
                 <div class="icon">
-                     <a href="<?php echo site_url('contact')?>"><img src="<?php echo base_url();?>media/img/daftar.png" class="img-rounded" width="80"></a>
+                     <a href="#"><img src="<?php echo base_url();?>media/img/daftar.png" class="img-rounded" width="80"></a>
                 </div>
                 <div class="text">
                     <var><?php echo $sum_contact?></var>
                     <label class="text-muted">Jumlah kontak</label>
                 </div>
-                
+                <a href="<?php echo site_url('contact')?>" class="btn btn-primary" title="Enlace">Lihat Kontak »</a>
             </div>
 		</div>
-        <div class="col-sm-3">
+        <div class="col-md-4">
             <div class="hero-widget well well-sm">
                 <div class="icon">
-                     <a href="<?php echo site_url('sms/inbox')?>"><img src="<?php echo base_url();?>media/img/inbox.png" class="img-rounded" width="70"></a>
+                     <a href="#"><img src="<?php echo base_url();?>media/img/inbox.png" class="img-rounded" width="70"></a>
                 </div>
                 <div class="text">
                     <var><?php echo $sum_inbox?></var>
                     <label class="text-muted">Jumlah Kotak Masuk</label>
                 </div>
-                
+                <a href="<?php echo site_url('sms/inbox')?>" class="btn btn-primary" title="Enlace">Lihat Pesan Masuk »</a>
             </div>
 		</div>
-        <div class="col-sm-3">
+        <div class="col-md-4">
             <div class="hero-widget well well-sm">
                 <div class="icon">
-                     <a href="<?php echo site_url('sms/sentitem')?>"><img src="<?php echo base_url();?>media/img/outbox.png" class="img-rounded" width="70"></a>
+                     <a href="#"><img src="<?php echo base_url();?>media/img/outbox.png" class="img-rounded" width="70"></a>
                 </div>
                 <div class="text">
                     <var><?php echo $sum_sentitem?></var>
                     <label class="text-muted">Jumlah Pesan Terkirim</label>
                 </div>
-                
+                <a href="<?php echo site_url('sms/sentitem')?>" class="btn btn-primary" title="Enlace">Lihat Pesan Terkirim »</a>
             </div>
     	</div>
 	</div>
 </div>
 
 
-
-	
