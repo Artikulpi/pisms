4db382499c4ff7c37bee61796559b1833b3bbc99
nazih14 <nazihbopas@gmail.com>
2014-06-06 17:27:50 +0700

    Update form sms

--- application/views/sms/inbox.php
+++ application/views/sms/inbox.php
@@ -33,31 +33,7 @@
 <div class="text-right"><?php echo $halaman?></div>
 
 
-		<div class="row">
-			<div id="myModal" class="modal fade in" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-	        <div class="modal-dialog">
-	            <div class="modal-content">
- 
-	                <div class="modal-header">
-	                    <a class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span></a>
-	                    <h4 class="modal-title" id="myModalLabel">Modal Heading</h4>
-	                </div>
-	                <div class="modal-body">
-						
-	                </div>
-	                <div class="modal-footer">
-	                    <div class="btn-group">
-	                    <button class="btn btn-danger" data-dismiss="modal">Cancel</button>
-	                    <button class="btn btn-primary">Save changes</button>
-	                </div>
-	                </div>
- 
-	            </div><!-- /.modal-content -->
-	        </div><!-- /.modal-dalog -->
-	</div><!-- /.modal -->
-    
-	<a data-toggle="modal" href="#myModal">Launch demo modal</a>
-		</div>
+		
 	
 	
 	
