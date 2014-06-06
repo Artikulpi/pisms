6dbb2f5bf1ff740373bd97f916d19c0030d62f51
nazih14 <nazihbopas@gmail.com>
2014-06-06 14:54:47 +0700

    Mengubah tampilan form sendto

--- application/views/sms/sendto.php
+++ application/views/sms/sendto.php
@@ -17,7 +17,7 @@ echo form_open('sms/create'); ?>
 <div class="form-group">
 	<label class="control-label col-sm-3">Isi *</label>
 	<div class="col-sm-9">
-		<textarea class="form-control" id="karakter" maxlength="160" name="content" rows="3" style="max-width:300px"></textarea>
+		<textarea class="form-control" id="karakter" maxlength="160" name="content" rows="3" style="max-width:1000px"></textarea><br>
 		<div style="width:264px;text-align: center" class="alert alert-danger"><span id="hitung">160</span> Karakter Tersisa.</div>
 	</div>
 </div>
@@ -25,11 +25,12 @@ echo form_open('sms/create'); ?>
 <div class="form-group">
 	<label class="control-label col-sm-3">Nomor Tujuan *</label>
 	<div class="col-sm-9">
-		<input type="text"class="form-control" name="contact" value="<?php echo $contact->phone_number;?>" style="max-width:300px">
+		<input type="text"class="form-control" name="contact" value="<?php echo $contact->phone_number;?>" style="max-width:1000px">
 	</div>
 </div>
 
-<div class="text-center">
+		<label class="control-label col-sm-3"></label>
+		<div class="col-sm-2">
 	<input type="submit" class="btn btn-success" value="Kirim"/>
-</div>
+</div></div>
 <?php echo form_close(); ?>
