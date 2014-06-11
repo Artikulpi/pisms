37e16fef0c6083c22723538b4a1e08d86e9bfc85
nazih14 <nazihbopas@gmail.com>
2014-06-11 23:17:49 +0700

    Update tampilan detail inbox

--- application/views/inbox/detailInbox.php
+++ application/views/inbox/detailInbox.php
@@ -1,14 +1,41 @@
-<span class="label label-success">Pengirim</span> <?php echo $sms->SenderNumber;?>
-<br>
-<span class="label label-success">Tanggal</span> <?php echo $sms->ReceivingDateTime;?>
-<br>
-<span class="label label-info">Isi</span>
+
+
+
+
+            <div class="row">
+                <div class="col-sm-12"> 
+<!-- Begin Listing: 218 LYNNEBROOK LN-->
+                    <div class="brdr bgc-fff pad-10 box-shad btm-mrg-20 property-listing">
+                        <div class="media">
+                            <a class="pull-left" href="#" target="_parent">
+                           
+
+                            <div class="clearfix visible-sm"></div>
+
+                            <div class="media-body fnt-smaller">
+                                <a href="#" target="_parent"></a>
+
+                                <h4 class="media-heading">
+                                  <a href="#" target="_parent">Pengirim <small><?php echo $sms->SenderNumber;?></small> <span class="fnt-smaller fnt-lighter fnt-arial"></span></a></h4><br>
+								  <ul class="list-inline mrg-0 btm-mrg-10 clr-535353">
+								                                      <li><a href="#" target="_parent"><h4 class="media-heading">Tanggal <small><?php echo $sms->ReceivingDateTime;?></small></a></h4></li>
+
+								                                    
+								                                  </ul>
+								  
+                                <p class="hidden-xs"><span class="label label-info">Isi</span>
 <div class="well well-sm">
 	<?php echo $sms->TextDecoded;?>
-</div></div>
-
-<div class="text-left">
+</div></p><hr><span class="fnt-smaller fnt-lighter fnt-arial"><div class="text-left">
 	<a href="<?php echo site_url('sms/reply/'.$sms->ID)?>" class="btn btn-success btn-xs"><span class="glyphicon glyphicon-share" data-toggle="tooltip" data-placement="bottom" title="Replay"></span></a>
 	<a href="<?php echo site_url('inbox/forward/'.$sms->ID)?>" class="btn btn-success btn-xs"><span class="glyphicon glyphicon-share-alt" data-toggle="tooltip" data-placement="bottom" title="Forward"></span></a>
 	<a href="<?php echo site_url('inbox/delete/'.$sms->ID)?>" class="btn btn-success btn-xs" onclick="return confirm('Are you sure you want to delete this item?');"><span class="glyphicon glyphicon-trash" data-toggle="tooltip" data-placement="bottom" title="Hapus"><span></a>
-</div>
+</div></span>
+                            </div>
+                        </div>
+                    </div><!-- End Listing-->
+
+                   
+                    <!-- Begin Listing: 704 SAINT GEORGES ST-->
+                    
+                </div>
