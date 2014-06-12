eeff703a4f84c182678bc6dd8f31742d22b530c1
nazih14 <nazihbopas@gmail.com>
2014-06-12 16:36:05 +0700

    Style inbox

--- application/views/outbox/outbox.php
+++ application/views/outbox/outbox.php
@@ -4,36 +4,38 @@
 		$onclick = array('onclick'=>"return confirm('Anda yakin ingin menghapus?')");
 		$delete = anchor('outbox/delete/'.$row->ID,'<span class="btn btn-xs btn-success"><span class="glyphicon glyphicon-trash" data-toggle="tooltip" data-placement="bottom" title="Hapus"></span></span>', $onclick);
 		?>
-		<?php
-	}
-	?>
+		
 
-            <div class="row">
-                <div class="col-sm-12"> 
-<!-- Begin Listing: 218 LYNNEBROOK LN-->
-                    <div class="brdr bgc-fff pad-10 box-shad btm-mrg-20 property-listing">
-                        <div class="media">
-                            <a class="pull-left" href="#" target="_parent">
-                           
-
-                            <div class="clearfix visible-sm"></div>
-
-                            <div class="media-body fnt-smaller">
-                                <a href="#" target="_parent"></a>
-
-                                <h4 class="media-heading">
-                                  <a href="#" target="_parent">@_<?php echo $row->DestinationNumber;?> <span class="fnt-smaller fnt-lighter fnt-arial"></span><small class="pull-right"></small></a></h4><br>
-								  <ul class="list-inline mrg-0 btm-mrg-10 clr-535353">
-								                                      <li><?php echo $row->SendingDateTime;//date ("D, d M Y H:i:s",strtotime($row->SendingDateTime));?></</li></ul>
-								  
-                                <h5><p class="hidden-xs"><?php echo $row->TextDecoded;?></p></h5><hr><span class="fnt-smaller fnt-lighter fnt-arial"><?php echo $delete.' hapus';?></span>
-                            </div>
-                        </div>
-                    </div><!-- End Listing-->
-
-                   
-                    <!-- Begin Listing: 704 SAINT GEORGES ST-->
-                    
-                </div>
+				<div class="row">
+				</div>
+				<div class="message-item" id="m1">
+					<div class="message-inner">
+						<div class="message-head clearfix">
+							<div class="avatar pull-left">
+								<a href="./index.php?qa=user&qa_1=admin"><img src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png"></a>
+							</div>
+							<div class="user-detail">
+							<h5 class="handle">@_<?php echo $row->DestinationNumber;?></h5>
+									<div class="post-meta">
+										<div class="asker-meta">
+											<span class="qa-message-what"></span>
+												<span class="qa-message-when">
+													<span class="qa-message-when-data"><?php echo $row->SendingDateTime;//date ("D, d M Y H:i:s",strtotime($row->SendingDateTime));?></span>
+														</span>
+														<span class="qa-message-who">
+														<span class="qa-message-who-pad">by </span>
+														<span class="qa-message-who-data"><a href="./index.php?qa=user&qa_1=admin">admin</a></span>
+														</span>
+														</div>
+														</div>
+														</div>
+														</div>
+							<div class="qa-message-content"><?php echo $row->TextDecoded;?>
+							</div>
+							<p class="hidden-xs"></p><hr><span class="fnt-smaller fnt-lighter fnt-arial"><?php echo $delete.' hapus';?></p></span>
+											</div>
+											</div>
+				
+					<?php } ?>
 <div class="text-right"><?php echo $halaman?></div>
 
--- application/views/sentitem/sentitem.php
+++ application/views/sentitem/sentitem.php
@@ -43,44 +43,46 @@
 							</form>
 						
 			
-			<div class="row">
-				<div class="col-sm-12"> 
-					<!-- Begin Listing: 218 LYNNEBROOK LN-->
-					<div class="brdr bgc-fff pad-10 box-shad btm-mrg-20 property-listing">
-						<div class="media">
-							<a class="pull-left" href="#" target="_parent">
-
-
-								<div class="clearfix visible-sm"></div>
-
-								<div class="media-body fnt-smaller">
-									<a href="#" target="_parent"></a>
-
-									<h4 class="media-heading">
-	
+			
 	<?php
 	foreach ($sentitem as $row) { 
 		$onclick = array('onclick'=>"return confirm('Anda yakin ingin menghapus?')");
 		$delete = anchor('sentitem/delete/'.$row->ID,'<span class="btn btn-xs btn-success"><span class="glyphicon glyphicon-trash" data-toggle="tooltip" data-placement="bottom" title="Hapus"></span></span>', $onclick);
 		?>
-	
-			<a href="#" target="_parent">@_<?php echo $row->DestinationNumber;?><span class="fnt-smaller fnt-lighter fnt-arial"></span></a></h4><br>
-			<ul class="list-inline mrg-0 btm-mrg-10 clr-535353">
-				<li><?php echo $row->ReceivingDateTime;//date ("D, d M Y H:i:s",strtotime($row->ReceivingDateTime));?></li>
-			</ul>
 
-			<p class="hidden-xs"><?php echo $row->TextDecoded;?></td>
-			<td><?php echo $row->SendingDateTime;?></p><hr><span class="fnt-smaller fnt-lighter fnt-arial"><?php echo $reply.' replay '.$forward.' forward '.$delete.' hapus';?></span>
-							</div>
-						</div>
-					</div><!-- End Listing-->
-				</div>
-				
-		
+<div class="row">
+		</div>
+		<div class="message-item" id="m1">
+			<div class="message-inner">
+				<div class="message-head clearfix">
+					<div class="avatar pull-left">
+						<a href="./index.php?qa=user&qa_1=admin"><img src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png"></a>
+					</div>
+					<div class="user-detail">
+					<h5 class="handle">@_<?php echo $row->DestinationNumber;?></h5>
+							<div class="post-meta">
+								<div class="asker-meta">
+									<span class="qa-message-what"></span>
+										<span class="qa-message-when">
+											<span class="qa-message-when-data"><?php echo $row->SendingDateTime;//date ("D, d M Y H:i:s",strtotime($row->SendingDateTime));?></span>
+												</span>
+												<span class="qa-message-who">
+												<span class="qa-message-who-pad">by </span>
+												<span class="qa-message-who-data"><a href="./index.php?qa=user&qa_1=admin">admin</a></span>
+												</span>
+												</div>
+												</div>
+												</div>
+												</div>
+					<div class="qa-message-content"><?php echo $row->TextDecoded;?>
+					</div>
+					<p class="hidden-xs"></p><hr><span class="fnt-smaller fnt-lighter fnt-arial"><?php echo $delete.' hapus';?></p></span>
+									</div>
+									</div>
 
 		<?php
 	}
 	?>
-			</div></div></div></div>
+			
 						<div class="text-right"><?php echo $halaman?></div>
 
