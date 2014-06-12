0e319f9a51aa75c50f6030457a15c2830c5916d4
nazih14 <nazihbopas@gmail.com>
2014-06-12 23:49:37 +0700

    Update search form

--- application/views/inbox/inbox.php
+++ application/views/inbox/inbox.php
@@ -1,32 +1,12 @@
-					<small class="pull-right">
-					<a class="btn btn-primary" href="#primary" data-toggle="modal"><h5><i class="glyphicon glyphicon-search"></i> Search</h5></a><hr>
-					</small>
-    <!-- Modal -->
-    <div class="modal fade" id="primary" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header modal-header-primary">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                    <h1><i class="glyphicon glyphicon-thumbs-up"></i> Pencarian Pesan Masuk</h1>
-                </div>
-                <div class="modal-body">
-					<form class="form-inline navbar-right" role="form" action="<?php echo site_url('inbox/search')?>" role="search" method="POST">
-						<div class="form-group">
-							<div class="well well-sm">
-								<input type="text" name="cari" class="form-control" placeholder="Search">
-								<div class="col-sm-4">
-									<button type="submit" class="btn btn-default">Submit</button>
-								</div></small></div></div>
-							</form><br>
-                		</div>
-                		<div class="modal-footer">
-                    <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
-                </div>
-            </div><!-- /.modal-content -->
-        </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
-    <!-- Modal -->
-   
+				
+<form class="form-inline navbar-right" role="form" action="<?php echo site_url('inbox/search')?>" role="search" method="POST">
+	<div class="form-group">
+		<div class="well well-sm">
+			<input type="text" name="cari" class="form-control" placeholder="Search">
+			<div class="col-sm-4">
+				<button type="submit" class="btn btn-default">Submit</button>
+			</div></small></div></div>
+		</form>
 		<form action="<?php echo site_url('inbox/filter')?>" method="POST">
 			<div class="col-sm-2">
 				<select name="tgl" class="form-control">
--- application/views/sentitem/sentitem.php
+++ application/views/sentitem/sentitem.php
@@ -1,15 +1,4 @@
-					<small class="pull-right">
-					<a class="btn btn-primary" href="#primary" data-toggle="modal"><h5><i class="glyphicon glyphicon-search"></i> Search</h5></a><hr>
-					</small>
-    <!-- Modal -->
-    <div class="modal fade" id="primary" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header modal-header-primary">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                    <h1><i class="glyphicon glyphicon-thumbs-up"></i> Pencarian Pesan Terkirim</h1>
-                </div>
-                <div class="modal-body">
+
 					<form class="form-inline navbar-right" role="form" action="<?php echo site_url('sentitem/search')?>" role="search" method="POST">
 						<div class="form-group">
 							<div class="well well-sm">
@@ -17,16 +6,7 @@
 								<div class="col-sm-4">
 									<button type="submit" class="btn btn-default">Submit</button>
 								</div></small></div></div>
-							</form><br>
-                		</div>
-                		<div class="modal-footer">
-                    <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
-                </div>
-            </div><!-- /.modal-content -->
-        </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
-    <!-- Modal -->
-
+							</form>
 				<form action="<?php echo site_url('sentitem/filter')?>" method="POST">
 					<div class="col-sm-2">
 						<select name="tgl" class="form-control">
