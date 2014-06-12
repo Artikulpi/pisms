8a5aff44f727ae853c378f447857775e23e49d60
nazih14 <nazihbopas@gmail.com>
2014-06-12 14:53:07 +0700

    Perbaikan tampilan inbox

--- application/views/inbox/inbox.php
+++ application/views/inbox/inbox.php
@@ -58,10 +58,7 @@
 						<div class="message-head clearfix">
 							<div class="avatar pull-left"><a href="./index.php?qa=user&qa_1=admin"><img src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png"></a></div>
 							<div class="user-detail">
-			
-							
-											
-															<h5 class="handle">@_<?php echo $row->SenderNumber;?></h5>
+							<h5 class="handle">@_<?php echo $row->SenderNumber;?></h5>
 															<div class="post-meta">
 																<div class="asker-meta">
 																	<span class="qa-message-what"></span>
@@ -87,10 +84,8 @@
 									</div>
 								</div>
 							</div>
-							
-									<?php
-								}
-								?>
+							</div>
+									<?php }   ?>
 								<div class="text-right"><?php echo $halaman?></div>
 								
 								
