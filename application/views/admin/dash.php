f6182b3a84bda10e9f5d1d1ce41721dd52f02ffc
nazih14 <nazihbopas@gmail.com>
2014-06-16 16:28:02 +0700

    Update tampilan dashboard

--- application/views/admin/dash.php
+++ application/views/admin/dash.php
@@ -1,24 +1,77 @@
-    <div class="container-fluid">
+   
+<div class="container-fluid">
 	<div class="row">
+		
+		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+			<div class="offer offer-radius offer-primary">
+				<div class="shape">
+					<div class="shape-text">
+						<?php echo $sum_contact?>						
+					</div>
+				</div>
+				<div class="offer-content">
+					<h3 class="lead">
+						<div class="my_planPrice"><?php echo $sum_contact?></div>
+						Jumlah kontak
+					</h3>						
+					<p>
+		                <div class="col-sm-12	">
+		                    <a href="<?php echo site_url('contact')?>" type="button" class="btn btn-default">Lihat Kontak »</a><hr>
+		                </div>
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+			<div class="offer offer-radius offer-primary">
+				<div class="shape">
+					<div class="shape-text">
+						<?php echo $sum_inbox?>							
+					</div>
+				</div>
+				<div class="offer-content">
+					<h3 class="lead">
+						<div class="my_planPrice"><?php echo $sum_inbox?></div>
+						Jumlah pesan masuk
+					</h3>						
+					<p>
+		                <div class="col-sm-12">
+		                    <a href="<?php echo site_url('inbox')?>" type="button" class="btn btn-default">Lihat Pesan Masuk »</a><hr>
+		                </div>
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+			<div class="offer offer-radius offer-primary">
+				<div class="shape">
+					<div class="shape-text">
+						<?php echo $sum_sentitem?>							
+					</div>
+				</div>
+				<div class="offer-content">
+					<h3 class="lead">
+						<div class="my_planPrice"><?php echo $sum_sentitem?></div>
+						Jumlah pesan terkirim
+					</h3>						
+					<p>
+		                <div class="col-sm-12">
+		                    <a href="<?php echo site_url('sentitem')?>" type="button" class="btn btn-default">Lihat Pesan Terkirim »</a><hr>
+		                </div>
+					</p>
+				</div>
+			</div>
+		</div>	
         <div class="col-xs-12 col-md-12 col-sm-12">
             <div class="row">
-                <div class="col-sm-4 my_planHeader my_plan1">
-                    <div class="my_planTitle"><?php echo $sum_contact?></div>
-                    <div class="my_planPrice">Jumlah kontak</div>
-                    <a href="<?php echo site_url('contact')?>" type="button" class="btn btn-default">Lihat Kontak »</a>
-                </div>
-                <div class="col-sm-4 my_planHeader my_plan2">
-                    <div class="my_planTitle"><?php echo $sum_inbox?></div>
-                    <div class="my_planPrice">Jumlah Kotak Masuk</div>
-                    <a href="<?php echo site_url('inbox')?>" type="button" class="btn btn-default">Lihat Pesan Masuk »</a>
-                </div>
-                <div class="col-sm-4 my_planHeader my_plan3">
-                    <div class="my_planTitle"><?php echo $sum_sentitem?></div>
-                    <div class="my_planPrice">Jumlah Pesan Terkirim</div>
-                    <a href="<?php echo site_url('sentitem')?>" type="button" class="btn btn-default">Lihat Pesan Terkirim »</a>
-                </div>
+                
+                
+                
             </div>
-        </div>
-   </div>
+		
+	</div>
+</div>
 
 
+    
+        
