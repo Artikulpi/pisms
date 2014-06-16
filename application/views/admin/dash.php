96501ce2adca61a9d813910347c2cc997de3d256
nazih14 <nazihbopas@gmail.com>
2014-06-16 15:05:48 +0700

    Merubah tampilan dashboard

--- application/views/admin/dash.php
+++ application/views/admin/dash.php
@@ -1,18 +1,18 @@
     <div class="container-fluid">
 	<div class="row">
-        <div class="col-xs-12 col-sm-offset-0 col-sm-12">
+        <div class="col-xs-12 col-md-12 col-sm-12">
             <div class="row">
-                <div class="col-xs-4 my_planHeader my_plan1">
+                <div class="col-sm-4 my_planHeader my_plan1">
                     <div class="my_planTitle"><?php echo $sum_contact?></div>
                     <div class="my_planPrice">Jumlah kontak</div>
                     <a href="<?php echo site_url('contact')?>" type="button" class="btn btn-default">Lihat Kontak »</a>
                 </div>
-                <div class="col-xs-4 my_planHeader my_plan2">
+                <div class="col-sm-4 my_planHeader my_plan2">
                     <div class="my_planTitle"><?php echo $sum_inbox?></div>
                     <div class="my_planPrice">Jumlah Kotak Masuk</div>
                     <a href="<?php echo site_url('inbox')?>" type="button" class="btn btn-default">Lihat Pesan Masuk »</a>
                 </div>
-                <div class="col-xs-4 my_planHeader my_plan3">
+                <div class="col-sm-4 my_planHeader my_plan3">
                     <div class="my_planTitle"><?php echo $sum_sentitem?></div>
                     <div class="my_planPrice">Jumlah Pesan Terkirim</div>
                     <a href="<?php echo site_url('sentitem')?>" type="button" class="btn btn-default">Lihat Pesan Terkirim »</a>
