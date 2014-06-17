e0e59db1ce00afe19058986dd8cd49a8109f4515
nazih14 <nazihbopas@gmail.com>
2014-06-17 16:59:25 +0700

    Memperbarui tampilan checkbox

--- application/views/sms/editDraft.php
+++ application/views/sms/editDraft.php
@@ -17,8 +17,7 @@ $(document).ready(function() {
 <?php
 echo validation_errors();
 echo form_open('sms/create'); ?>
-	<div class="container-fluid">
-	    <div class="row">
+	
 	        <div class="col-md-12">
 	                <form>
 	                <div class="row">
@@ -28,82 +27,119 @@ echo form_open('sms/create'); ?>
 							<textarea class="form-control" id="karakter" maxlength="160" name="content" rows="3"><?php echo $draft->content;?></textarea><br>
 							<div style="width:264px;text-align: center" class="alert alert-warning"><span id="hitung">160</span> Karakter Tersisa.</div>
 	                        </div>
-	                    </div>
-						<label class="control-label col-sm-12">Kirim Berdasarkan :</label>
-							<div class="col-sm-12 col-md-6">
-						<div class="panel-group" id="accordion">
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title">
-										<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
-											Masukan Nomor Telepon
-										</a>
-									</h4>
-								</div>
-								<div id="collapseOne" class="panel-collapse collapse">
-									
-									<div class="panel-body">
-										<input type="text" class="form-control" name="number" />
-										<div class="text-left">
-											<div class="col-sm-3 col-md-8">
-											<input type="hidden" name="draft_id" value="<?php echo $draft->id;?>">
-											<input type="submit" name="input_manual" class="btn btn-default" value="Kirim"/>
-											<input type="submit" name="draft" class="btn btn-default" value="Simpan ke Draft"/>
-										</div>
-									</div>
-								</div>
-							</div></div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title">
-										<a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
-											Kontak
-										</a>
-									</h4>
-								</div>
-								<div id="collapseTwo" class="panel-collapse collapse">
-									<div class="panel-body">
-										<div class="control-group">
-										<?php foreach ($contact as $row) {?>
-										
-											<label>
-											<input name="contact" type="checkbox" value="<?php echo $row->phone_number?>"><?php echo $row->name?>
-											</label>
-										
-										<?php } ?></div><hr>
-										<div class="text-left">
-											<div class="col-sm-2 col-md-3">
-											<input type="submit" name="input_contact" class="btn btn-default" value="Kirim"/>
-										</div>
-									</div>
-								</div>
-							</div></div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title">
-										<a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
-											Grup
-										</a>
-									</h4>
-								</div>
-								<div id="collapseThree" class="panel-collapse collapse">
-									<div class="panel-body">
-										<?php foreach ($group as $row) {?>
-										
-											<div class="col-sm-4">
-												<input name="group" type="checkbox" value="<?php echo $row->id?>"> <?php echo $row->group_name?>
-											</div>
-										
-										<?php } ?></div>
-										<div class="text-left">
-											<div class="col-sm-2 col-md-2">
-											<span data-toggle="tooltip" data-placement="bottom" title="kirim"><input type="submit" name="input_group" class="btn btn-default" value="Kirim"><hr>
-										</div>
-									</div>
-								</div>
+	                    
+							<strong><h5>Multiple Select</h5></strong>
+							<select id="mySel3" class="form-control" multiple="multiple" style="width:400px;">
+								<optgroup label="Select multiple">
+									<option>One</option>
+									<option>Two</option>
+									<option>Three</option>
+									<option>Siz</option>
+									<option>Seven</option>
+									<option>Nine</option>
+								</optgroup>
+								<optgroup label="Select one" class="single">
+									<option>Four</option>
+									<option>Five</option>
+									<option>Six</option>
+								</optgroup>
+							</select>
+							<script type="text/javascript">
+							function(){
+
+								$("#mySel").select2({
+									allowClear:true
+								});
+
+								$("#mySel2").select2({
+									closeOnSelect:false
+								});
+
+								$("#mySel3").select2({
+									closeOnSelect:false
+								});
+
+								$('.single option').click(function() {
+							    // only affects options contained within the same optgroup
+							    // and doesn't include this
+							    $(this).siblings().prop('selected', false);
+							});
+							});
+
+							</script><br>
+
+							<div class="box">
+								<?php
+								echo form_open('sms/create');
+								?>
 							</div>
-						</div></div>
-	                </form>
-	            </div></div></div></div>
+	                        </div>
+	                    </div></div>
+							                  <label class="col-sm-12">Kirim Berdasarkan :</label>
+							                        <div class="form-group">
+													<div class="col-sm-12 col-md-6">
+												<div class="panel-group" id="accordion">
+													<div class="panel panel-default">
+														<div class="panel-heading">
+															<h4 class="panel-title">
+																<label>
+																	<input type="radio" name="optionsRadios" value="kontak" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" data-parent="#accordion"  id="optionsRadios1">
+																	Kontak
+																</lebel>
+															</h4>
+														</div>
+														<div id="collapseOne" class="panel-collapse collapse">
+															<div class="panel-body">
+																<div class="control-group">
+																<input type="text" class="form-control" name="fromcontact">
+															</div>
+														</div>
+													</div>
+												</div>
+													<div class="panel panel-default">
+														<div class="panel-heading">
+															<h4 class="panel-title">
+																<label>
+																	<input type="radio" name="optionsRadios" value="grup" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+																	Grup
+																</lebel>
+															</h4>
+														</div>
+														<div id="collapseTwo" class="panel-collapse collapse">
+															<div class="panel-body">
+																<?php foreach ($group as $row) {?>
+											
+																		<div class="col-sm-4">
+																		<input name="group" type="checkbox" value="<?php echo $row->id?>"> <?php echo $row->group_name?>	</div>
+										
+										
+																<?php } ?>
+																</div>
+															</div>
+														</div>
+							
+													<div class="panel panel-default">
+														<div class="panel-heading">
+															<h4 class="panel-title">
+																<label>
+																	<input type="radio" name="optionsRadios" value="kontak" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" data-parent="#accordion"  id="optionsRadios1">
+																	Menggunakan No. Telp
+																</label>
+															</h4>
+														</div>
+														<div id="collapseThree" class="panel-collapse collapse">
+															<div class="panel-body">
+																<input type="text" class="form-control" name="number" />
+															</div>
+														</div>
+													</div>
+												</div>
+							                </div>
+											<div class="col-sm-12">
+												<input type="submit" name="input_manual" class="btn btn-default" value="Kirim"/>
+											</div>
+							                </form>
+							            </div>
+						</div>
 	
 	<?php echo form_close(); ?>
