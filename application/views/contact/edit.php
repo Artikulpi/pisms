8b89c52e0ede7b58d9bbe3f318de992a10576fc9
nazih14 <nazihbopas@gmail.com>
2014-06-17 09:24:56 +0700

    Update checkbox contact

--- application/views/contact/edit.php
+++ application/views/contact/edit.php
@@ -23,8 +23,9 @@
 </div>
 
 <label class="control-label col-sm-3">Group</label>
-<div class="col-sm-3">
-	<div class="form-group">
+<div class="col-sm-9">
+	<div class="panel panel-default">
+	                <div class="panel-body">
 		<?php
 		foreach($chg as $x) {
 			$new[$x->group_id] = $x->group_id;
@@ -33,18 +34,18 @@
 			foreach ($group as $key) {
 				if (in_array($key->id, $new)) {
 					?>
-					<input type="checkbox" name="group[]" value="<?php echo $key->id;?>" checked><?php echo $key->group_name;?>
+					<input type="checkbox" name="group[]" value="<?php echo $key->id;?>" checked> <?php echo $key->group_name;?>
 					<?php
 				}else{
 					?>
-					<input type="checkbox" name="group[]" value="<?php echo $key->id;?>"><?php echo $key->group_name;?>
+					<input type="checkbox" name="group[]" value="<?php echo $key->id;?>"> <?php echo $key->group_name;?>
 					<?php
 				}
 			}
 		}else{
 			foreach ($group as $key) {
 				?>
-				<input type="checkbox" name="group[]" value="<?php echo $key->id;?>"><?php echo $key->group_name;?>
+				<input type="checkbox" name="group[]" value="<?php echo $key->id;?>"> <?php echo $key->group_name;?>
 				<?php
 			}
 		}
@@ -52,11 +53,11 @@
 
 	</div>
 </div>
-
+</div>
 <div class="text-left">
 	<label class="control-label col-sm-12"></label>
 	<div class="col-sm-2">
-		<input type="submit" class="btn btn-info" value="Edit"/>
+		<input type="submit" class="btn btn-default" value="Edit"/>
 	</div>
 </div>
 <?php echo form_close(); ?>
