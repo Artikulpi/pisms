f868ed2492710e7c8b81801c589c316eca2ba0f5
nazih14 <nazihbopas@gmail.com>
2014-06-17 17:15:15 +0700

    Memperbarui tampilan detail grup

--- application/views/pigroup/detail.php
+++ application/views/pigroup/detail.php
@@ -4,15 +4,17 @@
 		<th>Aksi</th>
 	</thead>
 	<tbody>
-		<?php
-		echo $group->group_name.'<br>';
+		<div class="panel panel-default">
+		                <div class="panel-body">
+		<font color="#080808"><h4>Nama Grup : <?php
+		echo $group->group_name.' ';
 		foreach ($chg as $key) {
 			$cehage = $key->contact_id;
 			$onclick = array('onclick'=>"return confirm('Anda yakin ingin menghapus?')");
 			$delete = anchor('contactgroup/delete/'.$key->id,'<span class="btn btn-xs btn-default"><span class="glyphicon glyphicon-trash" data-toggle="tooltip" data-placement="bottom" title="Hapus grup"> hapus</span></span>', $onclick);
 			foreach ($contact as $con) {
 
-				if($con->id == $cehage){ ?>
+				if($con->id == $cehage){ ?></h4></font></div></div>
 				<tr>
 					<td><span data-toggle="tooltip" data-placement="bottom" title="Detail grup"><i class="glyphicon glyphicon-user"></i> <?php echo anchor('contact/detail/'.$con->id, $con->name); ?></span></td>
 					<td><?php echo $delete; ?></td>
