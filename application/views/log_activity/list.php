06343e2027044646f17d1182ef4fc0b287d01f01
M Yusuf Bachtiar <ario@localhost.localdomain>
2014-06-19 17:07:18 +0700

    Simpan hasil oprekan autocomplete di git

--- application/views/log_activity/list.php
+++ application/views/log_activity/list.php
@@ -1,31 +1,33 @@
-<table class="table table-striped">
-	<thead>
-		<th>User</th>
-		<th>Aktifitas</th>
-		<th>Tanggal</th>
-		<th>Modul</th>
-	</thead>
-	<tbody>
-	<?php
-	foreach ($log as $row) { ?>
-	<tr>
-		<td><?php
-		foreach ($user as $key) {
-			if($key->id == $row->user_id){
-				echo $key->username;
-			}
-		}
-		$row->user_id;?>
-	</td>
-	<td><?php echo $row->activity;?></td>
-	<td><?php echo $row->date;//date ("D, d M Y H:i:s",strtotime($row->date));?></td>
-	<td><?php echo $row->module;?></td>
-</tr>
+<div id="checkboxlist">
+	<table class="table table-striped">
+		<thead>
+			<th>User</th>
+			<th>Aktifitas</th>
+			<th>Tanggal</th>
+			<th>Modul</th>
+		</thead>
+		<tbody>
+			<?php
+			foreach ($log as $row) { ?>
+			<tr>
+				<td><?php
+				foreach ($user as $key) {
+					if($key->id == $row->user_id){
+						echo $key->username;
+					}
+				}
+				$row->user_id;?>
+			</td>
+			<td><?php echo $row->activity;?></td>
+			<td><?php echo $row->date;//date ("D, d M Y H:i:s",strtotime($row->date));?></td>
+			<td><?php echo $row->module;?></td>
+		</tr>
 
-<?php
-}
-?>
+		<?php
+	}
+	?>
 </tbody>
 </table>
 
+</div>
 <div class="text-right"><?php echo $halaman?></div>
--- application/views/sms/add.php
+++ application/views/sms/add.php
@@ -1,96 +1,103 @@
 <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>media/css/jquery.tagsinput.css" />
 <script type="text/javascript" src="<?php echo base_url();?>media/js/jquery.tagsinput.js"></script>
-
+<script src="<?php echo base_url();?>media/js/chosen.jquery.js" type="text/javascript"></script>
+<script src="<?php echo base_url();?>media/js/prism.js" type="text/javascript" charset="utf-8"></script>
+<link rel="stylesheet" href="<?php echo base_url();?>media/css/style_ac.css">
+<link rel="stylesheet" href="<?php echo base_url();?>media/css/prism.css">
+<link rel="stylesheet" href="<?php echo base_url();?>media/css/chosen.css">
 <?php $this->load->view('sms/limiter')?>
 
-		<div class="row">
-			<div class="col-sm-12 col-md-12">
+<select data-placeholder="Choose a Country..." class="chosen-select" multiple style="width:1000px;" tabindex="2">
+	<?php foreach ($contact as $key) {
+		?>
+		<option value="<?php echo $key->id?>"><?php echo $key->name?></option>
+		<?php
+	}?>
+</select>
+<?php $this->load->view('sms/autocomplete')?>
+
+<div class="row">
+	<div class="col-sm-12 col-md-12">
+		<div class="form-group">
+			<?php echo validation_errors(); ?>
+			<Label>Pesan Anda</Label>
+			<?php
+			echo form_open('sms/create');
+			?>
+
+			<textarea id="limit" name="content" class="form-control" maxlength="160" rows="3"></textarea><br>
+			<div class="box">
+
+			</div>
+		</div>
+		<div class="col-sm-12 col-md-6">
+			<div class="row">
 				<div class="form-group">
-					<?php echo validation_errors(); ?>
-					<Label>Pesan Anda</Label>
-					<?php
-					echo form_open('sms/create');
-					?>
+					<label>Kirim Berdasarkan :</label>
+					<div class="panel-group" id="accordion">
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<h4 class="panel-title">
+									<label>
+										<input type="radio" name="optionsRadios" value="kontak" data-toggle="collapse" data-parent="#accordion"  id="optionsRadios1"  data-target="#wilayah">
+										Kontak
+									</label>
+								</h4>
+							</div>
+							<div id="wilayah" class="panel-collapse collapse">
+								<div class="panel-body">
+									<div class="control-group">
 
-					<textarea id="limit" name="content" class="form-control" maxlength="160" rows="3"></textarea><br>
-					<div class="box">
-
-					</div>
-				</div>
-				<div class="col-sm-12 col-md-6">
-					<div class="row">
-						<div class="form-group">
-							<label>Kirim Berdasarkan :</label>
-							<div class="panel-group" id="accordion">
-								<div class="panel panel-default">
-									<div class="panel-heading">
-										<h4 class="panel-title">
-											<label>
-												<input type="radio" name="optionsRadios" value="kontak" data-toggle="collapse" data-parent="#accordion"  id="optionsRadios1"  data-target="#wilayah">
-												Kontak
-											</label>
-										</h4>
-									</div>
-									<div id="wilayah" class="panel-collapse collapse">
-										<div class="panel-body">
-											<div class="control-group">
-												<input id="contact" type="text" class="tags" name="fromcontact" value="" />
-												<p class="text-right"><em>Gunakan tanda koma (,) sebagai pemisah</em></p>
-											</div>
-										</div>
-										<div class="control-group">
-											<span>&nbsp;</span>
-										</div>
 									</div>
 								</div>
+							</div>
+						</div>
 
-								<div class="panel panel-default">
-									<div class="panel-heading">
-										<h4 class="panel-title">
-											<label>
-												<input type="radio" name="optionsRadios" value="fromgroup" data-toggle="collapse" data-parent="#accordion" id="optionsRadios2" data-target="#partai">
-												Grup
-											</label>
-										</h4>
-									</div>
-									<div id="partai" class="panel-collapse collapse">
-										<div class="panel-body">
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<h4 class="panel-title">
+									<label>
+										<input type="radio" name="optionsRadios" value="fromgroup" data-toggle="collapse" data-parent="#accordion" id="optionsRadios2" data-target="#partai">
+										Grup
+									</label>
+								</h4>
+							</div>
+							<div id="partai" class="panel-collapse collapse">
+								<div class="panel-body">
+									<?php
+									if (!empty($group)) {
+										foreach ($group as $row):
+											?>
+										<div class="col-sm-4">
+											<input type="checkbox" value="<?php echo $row->id; ?>" name="groupcheck[]"> <?php echo $row->group_name; ?></div>
 											<?php
-											if (!empty($group)) {
-												foreach ($group as $row):
-													?>
-												<div class="col-sm-4">
-													<input type="checkbox" value="<?php echo $row->id; ?>" name="groupcheck[]"> <?php echo $row->group_name; ?></div>
-													<?php
-													endforeach;
-												}
-												?>
-											</div>
-										</div>
+											endforeach;
+										}
+										?>
 									</div>
+								</div>
+							</div>
 
 
-									<div class="panel panel-default">
-										<div class="panel-heading">
-											<h4 class="panel-title">
-												<label>
-													<input type="radio" name="optionsRadios" value="frominput" data-toggle="collapse" data-parent="#accordion" id="optionsRadios3" data-target="#phone">
-													Menggunakan No. Telp
-												</label>
-											</h4>
-										</div>
-										<div id="phone" class="panel-collapse collapse">
-											<div class="panel-body">
-												<div class="control-group">
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<label>
+											<input type="radio" name="optionsRadios" value="frominput" data-toggle="collapse" data-parent="#accordion" id="optionsRadios3" data-target="#phone">
+											Menggunakan No. Telp
+										</label>
+									</h4>
+								</div>
+								<div id="phone" class="panel-collapse collapse">
+									<div class="panel-body">
+										<div class="control-group">
 
-													<div class="control-group">
-														<input id="no_tujuan" type="text" class="tags" name="no_tujuan" value=""/>
-														<p class="text-right"><em>Gunakan tanda koma (,) sebagai pemisah</em></p>
-													</div>
-													<div class="control-group">
-														<span>&nbsp;</span>
-													</div>
-												</div>
+											<div class="control-group">
+												<input id="no_tujuan" type="text" class="tags" name="no_tujuan" value=""/>
+												<p class="text-right"><em>Gunakan tanda koma (,) sebagai pemisah</em></p>
+											</div>
+											<div class="control-group">
+												<span>&nbsp;</span>
 											</div>
 										</div>
 									</div>
@@ -99,15 +106,17 @@
 						</div>
 					</div>
 				</div>
+			</div>
+		</div>
 
-				<div class="col-md-12">
-					<div class="panel-footer">
-						<input type="submit" class="btn btn-default" value="Kirim">
-						<input type="submit" class="btn btn-default" name="draft" value="Simpak ke draf">
-					</div>
-				</div>
-			
-		
+		<div class="col-md-12">
+			<div class="panel-footer">
+				<input type="submit" class="btn btn-default" value="Kirim">
+				<input type="submit" class="btn btn-default" name="draft" value="Simpak ke draf">
+			</div>
+		</div>
+
+
 
 		<?php
 		form_close();
