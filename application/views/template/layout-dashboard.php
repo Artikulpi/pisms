8fa96bdf7e450fb3bfbb21f8fc3c30302f976fd1
nazih14 <nazihbopas@gmail.com>
2014-06-20 16:10:43 +0700

    Menghapus icon title

--- application/views/template/layout-dashboard.php
+++ application/views/template/layout-dashboard.php
@@ -9,7 +9,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="shortcut icon" href="<?php echo base_url(); ?>media/img/SMS.png">
+	
 	<link href="<?php echo base_url(); ?>media/css/style.css" rel="stylesheet" type='text/css'>
 	<link href="<?php echo base_url(); ?>media/css/bootstrap.min.css" rel="stylesheet">
 	<link href="<?php echo base_url(); ?>media/css/dashboard.css" rel="stylesheet">
