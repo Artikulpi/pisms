6dec3842e82bbbdc0f8607b7bd44e69b114eb532
Akhmat Safrudin <somat@artikulpi.com>
2014-06-20 14:12:45 +0700

    Update about page

--- application/views/about/about.php
+++ application/views/about/about.php
@@ -1,13 +1,7 @@
-	<link href="<?php echo base_url(); ?>media/css/bootstrap.min.css" rel="stylesheet">
-	<br>
-	<div class="text-center">
-		<h1>Aplikasi SMS Gateway berbasis WEB</h1>
-		<h1>piSMS versi 1.0</h1>
-	</div>
-	<p>Lisensi : GNU/GPL<br>
-		Kontak Pengembang : <a href="http://www.artikulpi.com" target="_blank">artikulpi.com</a><br>
-		Github : 
-		Jika terdapat bug pada aplikasi ini, maka sila kirim feedback pada link berikut <?php echo mailto('office@artikulpi.com', 'ini'); ?>
-	</p>
+<div class="text-center">
+	<h2>Aplikasi SMS Gateway berbasis WEB</h2>
+</div>
 
-
+<p>Kontak Pengembang : <a href="http://www.artikulpi.com" target="_blank">artikulpi.com</a></p>
+<p>Umpan balik : Kirim surel ke <?php echo mailto('office@artikulpi.com', 'pengembang'); ?></p>
+<p>Lisensi : GNU/GPL </p>
