b30527482ba1c6a7aec79a257f33f80045e44f1a
nazih14 <nazihbopas@gmail.com>
2014-06-20 16:09:27 +0700

    Update tampian inbox

--- application/views/inbox/inbox.php
+++ application/views/inbox/inbox.php
@@ -32,47 +32,44 @@ foreach ($inbox as $row) {
 	?>
 
 
-
 	<div class="message-item" id="m1">
 		<div class="message-inner">
 			<div class="message-head clearfix">
-				<div class="avatar pull-left"><img src="<?php echo base_url();?>media/img/images.jpeg">
+				<div class="avatar pull-left">
+					<img src="<?php echo base_url();?>media/img/images.jpeg">
 				</div>
 				<div class="user-detail">
-					<div class="post-meta">
-						<div class="asker-meta">
-							<span class="qa-message-when">
-								<?php
+					<font color="#080808"><h5 class="handle">
+						<?php
 								foreach ($contact as $key) {
 									if($key->phone_number == $row->SenderNumber){
 										echo anchor('contact/detail/'.$key->id, $key->name);
 									}
 								}
 								?>
-								<h5><?php echo $row->SenderNumber;?></h5>
-							</span>
+						<?php echo $row->SenderNumber;?></h5></font>
+						<div class="post-meta">
+							<div class="asker-meta">
+								<span class="qa-message-what"></span>
+								<span class="qa-message-when">
+									<span class="qa-message-when-data"><span class="glyphicon glyphicon-time"></span> <?php echo $row->ReceivingDateTime;//date ("D, d M Y H:i:s",strtotime($row->ReceivingDateTime));?></span>
+								</span>
+								<span class="qa-message-who">
 
-							<span class="qa-message-who">
-								<span class="qa-message-who-data"></span>
-							</span>
-						</div>
-						<h6>
-							<span class="qa-message-when-data"><span class="glyphicon glyphicon-time"></span>  <?php echo $row->ReceivingDateTime;//date ("D, d M Y H:i:s",strtotime($row->ReceivingDateTime));?></span></h6>
+									<span class="qa-message-who-data"><a href="./index.php?qa=user&qa_1=admin"></a></span>
+								</span>
+							</div>
 						</div>
 					</div>
 				</div>
-
-
 				<div class="qa-message-content"><?php
 				$cut = character_limiter(strip_tags($row->TextDecoded),10);
 				echo anchor('inbox/detail/'.$row->ID, $cut);
 				?>
+				</div>
+				<small class="pull-right"><?php echo $reply.' '.$forward.' '.$delete;?></small><br>
 			</div>
-			<small class="pull-right"><?php echo $reply.' '.$forward.' '.$delete;?></small><br>
-
 		</div>
-	</div>
-
 
 	<?php }   ?>
 	<div class="text-right"><?php echo $halaman?></div>
