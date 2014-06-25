d1e99b3b6d659beca176d3fa5a778439a9a63a10
M Yusuf Bachtiar <ario@localhost.localdomain>
2014-06-25 14:20:03 +0700

    Implementasi kirim autocomplete dari kontak ke seluruh form

--- application/views/inbox/forwardInbox.php
+++ application/views/inbox/forwardInbox.php
@@ -9,107 +9,95 @@
 <?php
 echo validation_errors();
 echo form_open('sms/create'); ?>
-<select name="fromcontact[]" data-placeholder="Pilih dari kontak" class="chosen-select" multiple style="width:1000px;" tabindex="2">
-	<?php foreach ($contact as $key) {
-		?>
-		<option value="<?php echo $key->phone_number;?>"><?php echo $key->name?></option>
-		<?php
-	}?>
-</select>
 <?php $this->load->view('sms/autocomplete')?>
 
 <div class="col-sm-12 col-sm-offset-12 col-md-10 col-md-offset-1 main">
-			<div class="form-group">
-				<?php echo validation_errors(); ?>
-				<Label>Isi *</Label>
-				<textarea class="form-control" id="limit" maxlength="160" name="content" rows="3"><?php echo $sms->TextDecoded;?></textarea>
-				<div class="box">
-					<?php
-					echo form_open('sms/create');
+	<div class="form-group">
+		<?php echo validation_errors(); ?>
+		<Label>Isi *</Label>
+		<textarea class="form-control" id="limit" maxlength="160" name="content" rows="3"><?php echo $sms->TextDecoded;?></textarea>
+		<div class="box">
+			<?php
+			echo form_open('sms/create');
+			?>
+		</div>
+	</div>
+
+	<label>Kirim Berdasarkan :</label>
+	<div class="panel-group" id="accordion">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h4 class="panel-title">
+					<label>
+						<input type="radio" name="optionsRadios" value="kontak" data-toggle="collapse" data-parent="#accordion"  id="optionsRadios1"  data-target="#wilayah">
+						Kontak
+					</label>
+				</h4>
+			</div>
+			<div id="wilayah" class="panel-collapse collapse">
+				<div class="panel-body">
+					<input id="kontak" size="50" name="fromcontact">
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">
+				<label>
+					<input type="radio" name="optionsRadios" value="fromgroup" data-toggle="collapse" data-parent="#accordion" id="optionsRadios2" data-target="#partai">
+					Grup
+				</label>
+			</h4>
+		</div>
+		<div id="partai" class="panel-collapse collapse">
+			<div class="panel-body">
+				<?php
+				if (!empty($group)) {
+					foreach ($group as $row):
+						?>
+					<div class="col-sm-4">
+						<input type="checkbox" value="<?php echo $row->id; ?>" name="groupcheck[]"> <?php echo $row->group_name; ?></div>
+						<?php
+						endforeach;
+					}
 					?>
 				</div>
 			</div>
-		
-		<label>Kirim Berdasarkan :</label>
-		<div class="panel-group" id="accordion">
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<h4 class="panel-title">
-						<label>
-							<input type="radio" name="optionsRadios" value="kontak" data-toggle="collapse" data-parent="#accordion"  id="optionsRadios1"  data-target="#wilayah">
-							Kontak
-						</label>
-					</h4>
-				</div>
-				<div id="wilayah" class="panel-collapse collapse">
-					<div class="panel-body">
+		</div>
+
+
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h4 class="panel-title">
+					<label>
+						<input type="radio" name="optionsRadios" value="frominput" data-toggle="collapse" data-parent="#accordion" id="optionsRadios3" data-target="#phone">
+						Menggunakan No. Telp
+					</label>
+				</h4>
+			</div>
+			<div id="phone" class="panel-collapse collapse">
+				<div class="panel-body">
+					<div class="control-group">
+
 						<div class="control-group">
-							<input id="contact" type="text" class="tags" name="fromcontact" value="" />
+							<input id="no_tujuan" type="text" class="tags" name="no_tujuan" value=""/>
 							<p class="text-right"><em>Gunakan tanda koma (,) sebagai pemisah</em></p>
 						</div>
-					</div>
-					<div class="control-group">
-						<span>&nbsp;</span>
-					</div>
-				</div>
-			</div>
-
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<h4 class="panel-title">
-						<label>
-							<input type="radio" name="optionsRadios" value="fromgroup" data-toggle="collapse" data-parent="#accordion" id="optionsRadios2" data-target="#partai">
-							Grup
-						</label>
-					</h4>
-				</div>
-				<div id="partai" class="panel-collapse collapse">
-					<div class="panel-body">
-						<?php
-						if (!empty($group)) {
-							foreach ($group as $row):
-								?>
-							<div class="col-sm-4">
-								<input type="checkbox" value="<?php echo $row->id; ?>" name="groupcheck[]"> <?php echo $row->group_name; ?></div>
-								<?php
-								endforeach;
-							}
-							?>
+						<div class="control-group">
+							<span>&nbsp;</span>
 						</div>
 					</div>
 				</div>
-
-
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<h4 class="panel-title">
-							<label>
-								<input type="radio" name="optionsRadios" value="frominput" data-toggle="collapse" data-parent="#accordion" id="optionsRadios3" data-target="#phone">
-								Menggunakan No. Telp
-							</label>
-						</h4>
-					</div>
-					<div id="phone" class="panel-collapse collapse">
-						<div class="panel-body">
-							<div class="control-group">
-
-								<div class="control-group">
-									<input id="no_tujuan" type="text" class="tags" name="no_tujuan" value=""/>
-									<p class="text-right"><em>Gunakan tanda koma (,) sebagai pemisah</em></p>
-								</div>
-								<div class="control-group">
-									<span>&nbsp;</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				</div>
 			</div>
-			<center><div class="col-md-12">
-				<div class="panel-footer">
-					<input type="submit" class="btn btn-default" value="Kirim">
-					<input type="submit" class="btn btn-default" name="draft" value="Simpan ke draf">
-				</div></center>
-			</div>
-			<?php echo form_close(); ?>
+		</div>
+	</div>
+</div>
+<center><div class="col-md-12">
+	<div class="panel-footer">
+		<input type="submit" class="btn btn-default" value="Kirim">
+		<input type="submit" class="btn btn-default" name="draft" value="Simpan ke draf">
+	</div></center>
+</div>
+<?php echo form_close(); ?>
